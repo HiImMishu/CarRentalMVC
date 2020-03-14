690afa2b00896102c97589e2f24f6dae65617df5
HiImMishu <michal.misiak@op.pl>
2020-03-14 21:38:34 +0100

    Main Page Mobile View

--- web/assets/js/header.js
+++ web/assets/js/header.js
@@ -1,17 +1,20 @@
 function onClickMenu() {
 
   var items = document.getElementsByClassName("item");
+  var nav = document.getElementById("navigation");
 
   var index = (items[0].classList.contains("itemVisible")) ? 4 : 0;
 
   var i = index;
+
+  if(index == 0)
+    nav.classList.add("dp");
 
   setTimeout(show, 0);
   setTimeout(show, 200);
   setTimeout(show, 400);
   setTimeout(show, 600);
   setTimeout(show, 800);
-
   function show()
   {
     items[i].classList.toggle("itemVisible");
@@ -22,5 +25,12 @@ function onClickMenu() {
     else {
       i--;
     }
+    if(index == 4 && i==-1)
+      setTimeout(removeDp, 200);
+  }
+
+  function removeDp()
+  {
+    nav.classList.remove("dp");
   }
 }
---
+++ web/assets/js/slider.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  slideIndex = 1;
+  auto = 1;
+  showSlides(slideIndex);
+});
+
+// Next/previous controls
+function plusSlides(n, a) {
+  auto = a;
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+  if(auto)
+  {
+    setTimeout(function() {
+      plusSlides(1, 1);
+    }, 5000);
+  }
+}
