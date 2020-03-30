6c7a69a14401e28e42053a51a6a33aa479796434
HiImMishu <michal.misiak@op.pl>
2020-03-30 23:30:59 +0200

    Profile Cotroller

--- src/Controllers/ProfileController.php
+++ src/Controllers/ProfileController.php
@@ -8,6 +8,24 @@ class ProfileController extends App
 {
   public function indexAction()
   {
-    return $this->render('Views/profile.html.twig', ['firstName' => $this->getSession()->get('firstName')]);
+    if(!$this->getSession()->has('id'))
+      return $this->render('Views/404.html.twig');
+
+    $reservationList = $this->getReservations();
+
+    return $this->render('Views/profile.html.twig', ['firstName' => $this->getSession()->get('firstName'), 'reservations' => $reservationList]);
+  }
+
+  public function getReservations()
+  {
+    $id = $this->getSession()->get('id');
+
+    $eM = $this->getEntityMenager();
+
+    $query = $eM->creAteQuery('SELECT r from Models\Reservation r where r.user = :id AND r.dateTo >= :date');
+    $query->setParameter('id', $id);
+    $query->setParameter('date', date("Y-m-d"));
+
+    return $query->getResult();
   }
 }
